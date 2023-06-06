3ab98ea51d6a9da745a5f2ec98fcb16a18358d12
AleKs <alexanderfranciscog@gmail.com>
2023-06-05 23:41:30 -0400

    added new export statements

--- src/routes.js
+++ src/routes.js
@@ -1,2 +1,3 @@
 export { default as Section } from "./components/Section/Section"
 export { default as TestOptions } from "./components/TestOptions/TestOptions"
+export { default as Bubble } from "./components/Bubble/Bubble"
