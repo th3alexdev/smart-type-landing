d1e69e35d0bd06ccf6320e5f80e1137f47978c71
AleKs <alexanderfranciscog@gmail.com>
2023-06-05 23:32:08 -0400

    refactor component to include new props

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-function Section({ sectionId, title, children }) {
+function Section({ sectionId, title, children, customClass, titleClass }) {
   return (
-    <section id={ sectionId }>
+    <section className={`py-12 px-4 max-w-3xl mx-auto ${ customClass }`} id={ sectionId }>
       {
-        title && <h1>{ title }</h1>
+        title && <h1 className={`relative mb-3 text-lg md:text-2xl font-semibold ${titleClass} w-max px-5`}>
+          { title } 
+        </h1>
       }
       { children }
     </section>
