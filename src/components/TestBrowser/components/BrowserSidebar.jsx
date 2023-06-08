d35da51bfe901dd52d97c7cd0ef1f31777b418e9
AleKs <alexanderfranciscog@gmail.com>
2023-06-07 23:44:45 -0400

    refactored component and improve responsive behavior

--- src/components/TestBrowser/components/BrowserSidebar.jsx
+++ src/components/TestBrowser/components/BrowserSidebar.jsx
@@ -5,7 +5,7 @@ function BrowserSidebar({ test }) {
     <>
       {
         test === 1 && (
-          <div className='w-1/4 h-full bg-chatgpt-sidebar-pri py-2 px-3'>
+          <div className='hidden md:block w-1/4 h-full bg-chatgpt-sidebar-pri py-2 px-3'>
               <div className='w-full h-9 bg-chatgpt-sidebar-sec mb-4 rounded-sm'></div>
               <div className='w-full h-9 bg-chatgpt-sidebar-sec mb-2 rounded-sm'></div>
               <div className='w-full h-9 bg-chatgpt-sidebar-sec mb-2 rounded-sm'></div>
@@ -15,7 +15,7 @@ function BrowserSidebar({ test }) {
 
       {
         test === 2 && (
-           <div className='w-1/4 bg-ig-sidebar-pri py-2 border-r border-slate-400'>
+           <div className='hidden md:block w-1/4 bg-ig-sidebar-pri py-2 border-r border-slate-400'>
               <div className='w-full h-9 bg-ig-sidebar-pri mb-1 rounded-sm flex items-center justify-between px-2 gap-2'>
                   <div className='w-8 h-8 bg-ig-sidebar-sec rounded-full'></div>
                   <div className='w-20'>
@@ -78,28 +78,28 @@ function BrowserSidebar({ test }) {
     
       {
         test === 3 && (
-          <div className='w-4/12 bg-ig-sidebar-pri py-4 border-r border-slate-200'>
+          <div className='hidden md:block w-4/12 bg-ig-sidebar-pri py-4 border-r border-slate-200'>
               <div className='w-24 h-10 ml-2 mb-5 bg-mail-sidebar-sec rounded-xl'></div>
               <div className='w-full px-4 flex flex-col gap-4'>
-                  <div className='w-20 h-3 mail-sidebar-thi rounded-full'></div>
-                  <div className='w-24 h-3 mail-sidebar-thi rounded-full'></div>
-                  <div className='w-16 h-3 mail-sidebar-thi rounded-full'></div>
-                  <div className='w-22 h-3 mail-sidebar-thi rounded-full'></div>
+                  <div className='w-20 h-3 bg-mail-sidebar-thi rounded-full'></div>
+                  <div className='w-24 h-3 bg-mail-sidebar-thi rounded-full'></div>
+                  <div className='w-16 h-3 bg-mail-sidebar-thi rounded-full'></div>
+                  <div className='w-22 h-3 bg-mail-sidebar-thi rounded-full'></div>
 
                   <div className='w-full h-max pl-4 flex gap-2'>
-                      <div className='w-4 h-3 mail-sidebar-thi rounded-full'></div>
-                      <div className='w-full h-3 mail-sidebar-thi rounded-full'></div>
+                      <div className='w-4 h-3 bg-mail-sidebar-thi rounded-full'></div>
+                      <div className='w-full h-3 bg-mail-sidebar-thi rounded-full'></div>
                   </div>
                   <div className='w-full h-max pl-4 flex gap-2'>
-                      <div className='w-4 h-3 mail-sidebar-thi rounded-full'></div>
+                      <div className='w-4 h-3 bg-mail-sidebar-thi rounded-full'></div>
                       <div className='w-full h-3 bg-ig-sidebar-sec rounded-full'></div>
                   </div>
                   <div className='w-full h-max pl-4 flex gap-2'>
-                      <div className='w-4 h-3 mail-sidebar-thi rounded-full'></div>
+                      <div className='w-4 h-3 bg-mail-sidebar-thi rounded-full'></div>
                       <div className='w-full h-3 bg-ig-sidebar-sec rounded-full'></div>
                   </div>
 
-                  <div className='w-24 h-3 mail-sidebar-thi rounded-full'></div>
+                  <div className='w-24 h-3 bg-mail-sidebar-thi rounded-full'></div>
               </div>
           </div>          
         )
