056468191f2ea2e65c0971e580623ced890a6cb2
AleKs <alexanderfranciscog@gmail.com>
2023-06-07 23:49:19 -0400

    added new exports

--- src/components/TestBrowser/components/routes.js
+++ src/components/TestBrowser/components/routes.js
@@ -1,3 +1,4 @@
 export { default as BrowserHeader } from "./BrowserHeader" 
 export { default as BrowserSidebar } from "./BrowserSidebar" 
+export { default as BrowserTab } from "./BrowserTab" 
 export { default as BrowserTextarea } from "./BrowserTextarea" 
