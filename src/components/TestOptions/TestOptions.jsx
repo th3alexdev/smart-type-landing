3c49f38281d57aef8fb76518a305ac6fe958b2f8
AleKs <alexanderfranciscog@gmail.com>
2023-06-08 23:16:01 -0400

    updated component with dynamic test options

--- src/components/TestOptions/TestOptions.jsx
+++ src/components/TestOptions/TestOptions.jsx
@@ -1,42 +1,48 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-export default function TestOptions({ test, setTest }) {
+export default function TestOptions({ test, setTest, toggleOptions }) {
+
+  const options = [
+    {
+      id: 1,
+      label: 'Specific message for ChatGPT to do a certain task',
+      ariaLabel: 'Improve text writing prompts in ChatGPT',
+    },
+    {
+      id: 2,
+      label: 'Quick response in your online store for a client',
+      ariaLabel: 'Automate responses in your Instagram Store',
+    },
+    {
+      id: 3,
+      label: 'Send a repetitive email to your teamwork',
+      ariaLabel: 'Send a repetitive email to your teamwork',
+    },
+  ];
+  
+  const handleClick = (id) => {
+    setTest(id);
+    toggleOptions();
+  };
 
   return (
     <>
         <h2 className='mb-2  text-center font-medium'>Choose one!</h2>
         <div className='h-max relative mb-6 md:mb-8'>
           <ul className='relative z-20 mx-auto flex flex-col sm:flex-row gap-3 md:gap-5 text-center max-w-2xl'>
-            <li className='sm:w-1/3 md:w-max'>
-              <button 
-                className={`min-w-full sm:w-full md:w-auto min-h-full px-2 py-2 ${test === 1 ? 'bg-yellow' : 'bg-btnGray bg-lines bg-fill'}`}
-                aria-label='Improve text writing prompts in ChatGPT'
-                onClick={() => { 
-                  setTest(1) 
-                }}>
-                Specific message for ChatGPT to do a certain task 
-              </button>
-            </li>
-            <li className='sm:w-1/3 md:w-max'>
-              <button 
-                className={`min-w-full sm:w-full md:w-auto min-h-full px-2 py-2 ${test === 2 ? 'bg-yellow' : 'bg-btnGray bg-lines bg-fill'}`}
-                aria-label='Automate responses in your Instagram Store'
-                onClick={() => { 
-                  setTest(2) 
-                }}>
-                Quick response in your online store for a client
-              </button>
-            </li>
-            <li className='sm:w-1/3 md:w-max'>
-              <button 
-                className={`min-w-full sm:w-full md:w-auto min-h-full px-2 py-2 ${test === 3 ? 'bg-yellow' : 'bg-btnGray bg-lines bg-fill'}`}
-                aria-label='Send a repetitive message in LinkedIn'
-                onClick={() => { 
-                  setTest(3) 
-                }}>
-                Send a repetitive email to your teamwork
-              </button>
-            </li>
+            {
+              options.map((option) => (
+                <li className='sm:w-1/3 md:w-max' key={ option.id }> 
+                  <button 
+                    className={`min-w-full sm:w-full md:w-auto min-h-full px-2 py-2 ${test === option.id ? 'bg-yellow' : 'bg-btnGray bg-lines bg-fill'}`}
+                    aria-label={ option.ariaLabel }
+                    onClick={ () => handleClick(option.id) }
+                    >
+                    { option.label }
+                  </button>
+                </li>
+              ))
+            }
           </ul>
         <div className='hidden md:block absolute z-10 inset-y-1/2 w-full h-0.5 bg-[#B3B3B3]'></div>
       </div>
