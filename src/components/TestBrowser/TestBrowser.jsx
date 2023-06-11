49902b51cb42437e04b024303d4356134f454598
AleKs <alexanderfranciscog@gmail.com>
2023-06-10 23:35:17 -0400

    updated component with ARIA attributes

--- src/components/TestBrowser/TestBrowser.jsx
+++ src/components/TestBrowser/TestBrowser.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import { BrowserHeader, BrowserSidebar, BrowserTextarea, BrowserTab } from "./components/routes"
+import React, { useRef, useContext } from 'react'
+import { BrowserHeader, BrowserSidebar, BrowserTextarea, BrowserTab } from './components/routes'
+import { ManageContext } from '../../contexts/ContextProvider';
 
-function TestBrowser({ test, setTest, inputValue, setInputValue, toggleOptions, command }) {    
-      
+function TestBrowser({ }) {    
+
+    const { 
+        test, 
+        setTest,
+        handleToggleOptions,
+    } = useContext(ManageContext)
+
+
+    const browserContainerRef = useRef('null')    
+
     const tabs = [
         {
           imgPath: './../src/assets/test_browser/chatgpt.webp',
@@ -25,22 +35,25 @@ function TestBrowser({ test, setTest, inputValue, setInputValue, toggleOptions, 
     const renderContent = {
         1: (
             <>
-                <BrowserTextarea test={ test } setInputValue={ setInputValue } inputValue={ inputValue } command={ command }/>
+                <BrowserTextarea containerRef={ browserContainerRef } 
+                role='tabpanel' aria-labelledby="tab-content-1"/>
             </>
         ),
         2: (
             <>
-                <div className="w-full h-full flex flex-col">
+                <div className='w-full h-full flex flex-col' role="tabpanel" 
+                aria-labelledby="tab-content-1">
                     <BrowserHeader test={ test } />
-                    <BrowserTextarea test={ test } setInputValue={ setInputValue } inputValue={ inputValue }  command={ command }/>
+                    <BrowserTextarea containerRef={ browserContainerRef } />
                 </div>                
             </>
         ),
         3: (
             <>
-                <div className='h-full w-full flex flex-col justify-between'>
+                <div className='h-full w-full flex flex-col justify-between'
+                role="tabpanel" aria-labelledby="tab-content-1">
                     <BrowserHeader test={ test } />
-                    <BrowserTextarea test={ test } setInputValue={ setInputValue } inputValue={ inputValue } command={ command }/>
+                    <BrowserTextarea containerRef={ browserContainerRef } />
                     <div className='w-full h-12 px-4 flex items-center border-t border-slate-200'>
                         <div className='w-16 h-8 mr-3 bg-mail-sidebar-sec rounded-full'></div>    
                         <div className='w-6 h-6 mr-2 bg-mail-sidebar-thi rounded-sm'></div>    
@@ -55,11 +68,15 @@ function TestBrowser({ test, setTest, inputValue, setInputValue, toggleOptions, 
         )
     }
 
-
   return (
-    <div className='bg-white w-full h-[495px] rounded-t-lg overflow-hidden'>
-        <div className='h-8 bg-tabs-bar flex'>
-            <img src='./../src/assets/test_browser/browser-buttons.svg' className='mx-3 w-10' alt='' />
+    <div 
+        className='bg-white w-full h-[495px] rounded-t-lg overflow-hidden shadow-lg'
+        ref={ browserContainerRef }
+        role='region'
+        aria-label='Browser'
+    >
+        <div className='h-8 bg-tabs-bar flex' role='tablist' aria-label='Browser Tabs'>
+            <img src='./../src/assets/test_browser/browser-buttons.svg' className='mx-3 w-10' alt='trhee macOS window buttons' />
             {
             tabs.map((tab, index) => (
             <BrowserTab
@@ -69,16 +86,17 @@ function TestBrowser({ test, setTest, inputValue, setInputValue, toggleOptions, 
                 isSelected={test === tab.testValue}
                 onClick={ () => {
                     setTest(tab.testValue) 
-                    toggleOptions()  
+                    handleToggleOptions()  
                 }} 
                 customClass={ tab.testValue === 3 ? 'w-4 h-3' : null }
+                aria-controls={`tab-content-${tab.testValue}`}
             />
             ))
             }
         </div>
         <div className='h-8 bg-white border-b border-slate-200'></div>
 
-        <div className="h-browser-calc flex"> 
+        <div className='h-browser-calc flex' role='group'> 
             <BrowserSidebar test={test} />
             { renderContent[test] } 
         </div>
