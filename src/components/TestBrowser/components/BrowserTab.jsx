4099c1f642704c4edaf4c7565d8e4f3d503724ef
AleKs <alexanderfranciscog@gmail.com>
2023-06-10 23:34:25 -0400

    updated component with ARIA attributes for tab selection and focusability

--- src/components/TestBrowser/components/BrowserTab.jsx
+++ src/components/TestBrowser/components/BrowserTab.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 function BrowserTab({ isSelected, text, imgPath, customClass, onClick }) {
   return (
     <div 
-        className={`div-tab w-11 sm:w-max sm:pr-16 ${isSelected ? 'bg-white' : ''}`}
-        onClick={ (e) => { onClick(e) }}
+      className={`div-tab w-11 sm:w-max sm:pr-16 ${isSelected ? 'bg-white' : ''}`}
+      onClick={ (e) => { onClick(e) }}
+      role='rab'
+      aria-selected={ isSelected }
+      tabIndex={ isSelected ? 0 : -1 }
     >
         <img src={ imgPath } className={`${customClass ? customClass : 'w-4 h-4' }`} alt='' />
         <p className='text-xs hidden sm:block'>{ text }</p>
