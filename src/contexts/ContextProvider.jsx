958a5338f477acf9fc3e38d91f9b59886e1af86f
AleKs <alexanderfranciscog@gmail.com>
2023-06-10 23:22:50 -0400

    added state variables and functions

--- src/contexts/ContextProvider.jsx
+++ src/contexts/ContextProvider.jsx
@@ -1,11 +1,26 @@
 import React, { createContext, useState, useEffect } from 'react'
+import { COMMANDS } from './../constants/commands'
 export const ManageContext = createContext();
 
 export const ContextProvider = ({ children }) => {
-  const uwu = 'uwu'
+
+  const [test, setTest] = useState(1);
+  const [inputValue, setInputValue] = useState('');
+  const command = COMMANDS[test].SHORTCUT;
+
+  const handleToggleOptions = () => {
+    setInputValue('')
+  }
 
   return (
-    <ManageContext.Provider value={{ uwu }}>
+    <ManageContext.Provider value={{ 
+      command,
+      test, 
+      setTest,
+      inputValue,
+      setInputValue,
+      handleToggleOptions
+    }}>
       { children }
     </ManageContext.Provider>
   )
