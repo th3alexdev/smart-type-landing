09b65650064aee0c3b3b6a37b1e3d90e3ff02d62
AleKs <alexanderfranciscog@gmail.com>
2023-06-12 21:00:48 -0400

    modified image paths

--- src/components/Bubble/Bubble.jsx
+++ src/components/Bubble/Bubble.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
+import avatarImg from './../../assets/webp/avatar.png'
+import stuckOutTongueEmoji from './../../assets/webp/face-with-stuck-out-tongue.webp'
+import cameraEmoji from './../../assets/webp/camera.webp'
+import babyChickEmoji from './../../assets/webp/baby_chick.webp'
+import ghostEmoji from './../../assets/webp/ghost.webp'
+import bubbleSvg from './../../assets/webp/bubble.svg'
+
 function Bubble({ mainText, user, customClass }) {
   return (
     <div className={`flex items-end mb-8 md:mb-0 gap-2 justify-center ${ customClass }`}
       role='group' aria-label='Bubble'
     >
       <img 
-        src='./../src/assets/webp/avatar.png'
+        src={ avatarImg }
         alt='User Avatar'
         className='w-12 h-max rounded-full order-2 max-h-md'
         aria-hidden='true'
@@ -16,7 +23,7 @@ function Bubble({ mainText, user, customClass }) {
           <p className='flex gap-2 mb-2 text-lg'>
             { mainText }
             <img 
-              src='./../src/assets/webp/face-with-stuck-out-tongue.webp' alt='sticking out tongue face emoji'
+              src={ stuckOutTongueEmoji } alt='sticking out tongue face emoji'
               className='w-7' 
               aria-hidden='true'
             />
@@ -24,7 +31,7 @@ function Bubble({ mainText, user, customClass }) {
           <p className='flex gap-2 text-sm'>
             { user }
             <img 
-              src='./../src/assets/webp/camera.webp' alt='camera emoji'
+              src={ cameraEmoji } alt='camera emoji'
               className='w-4'
               aria-hidden='true'
             />
@@ -32,7 +39,7 @@ function Bubble({ mainText, user, customClass }) {
           <p className='flex gap-2 text-sm'>
             { user }
             <img 
-              src='./../src/assets/webp/baby_chick.webp' alt='baby_chick emoji'
+              src={ babyChickEmoji } alt='baby_chick emoji'
               className='w-4' 
               aria-hidden='true'
             />
@@ -40,14 +47,14 @@ function Bubble({ mainText, user, customClass }) {
           <p className='flex gap-2 text-sm'>
             { user }
             <img 
-              src='./../src/assets/webp/ghost.webp' alt='ghost emoji'
+              src={ ghostEmoji } alt='ghost emoji'
               className='w-4' 
               aria-hidden='true'
             />
           </p>
         </div>
         <img 
-          src='./../src/assets/webp/bubble.svg'
+          src={ bubbleSvg }
           alt='white dialog bubble' 
           className='h-40'
           aria-hidden='true'
