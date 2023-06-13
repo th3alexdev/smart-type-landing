df28f919e12e1b6d59c27e36793c4e68e5ac20ce
AleKs <alexanderfranciscog@gmail.com>
2023-06-12 21:01:11 -0400

    modified classes

--- src/constants/commands.js
+++ src/constants/commands.js
@@ -2,12 +2,12 @@ export const COMMANDS = Object.freeze({
     1: {
         SHORTCUT: '/prompt',
         EXPANSION: 'Hey AI assistant, could you kindly provide me with a comprehensive step-by-step guide on how to [insert your task!]?',
-        TEXTAREA_STYLES: ['h-48', '2xs:h-24', 'sm:h-[4.5rem]']
+        TEXTAREA_STYLES: ['h-48', '2xs:h-24', 'sm:h-expanded-fst']
     },
     2: {
         SHORTCUT: '/quick',
         EXPANSION: "Hello 👋! Thank you for reaching out to us. Here's a quick response to your inquiry about the prices of specific products: \n\n - [Product 1]: $[Price 1] \n - [Product 2]: $[Price 2] \n - [Product 3]: $[Price 3] \n\n 🙏 If you have any further questions, feel free to let us know. We're here to assist you! Happy shopping! 🛍️",
-        TEXTAREA_STYLES: ['xs:h-[22rem]', '3xs:h-[21rem]', '4xs:h-72', '5xs:h-[16.5rem]', '6xs:h-[16.5rem]', '7xs:h-[14.8rem]']
+        TEXTAREA_STYLES: ['xs:h-expanded-snd_1', '3xs:h-expanded-snd_2', '4xs:h-72', '5xs:h-expanded-snd_3', '6xs:h-expanded-snd_3', '7xs:h-expanded-snd_4']
     },
     3: {
         SHORTCUT:'/mail',
